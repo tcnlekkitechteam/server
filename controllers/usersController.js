d25524b9ed25e5953d7314a2bc47903b93930b5e
clintfidel <clintfidel@gmail.com>
2024-03-30 02:07:40 +0100

    add department name

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -71,12 +71,12 @@ const getUserById = async (req, res) => {
     const { userId } = req.params;
 
     const user = await User.findById(userId);
-
+    const department = await Department.findOne({ _id: req.params.id }).exec();
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
 
-    res.status(200).json({ user });
+    res.status(200).json({ ...user,  department: { name: department?.name ? department?.name : null} });
   } catch (err) {
     console.error('Error fetching user:', err);
     res.status(500).json({ error: 'Internal Server Error' });
