3a84926cd89e48827ac9162e8787a1adcead69f5
Austin Asoluka <stracool9@gmail.com>
2024-04-26 22:25:53 +0200

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,8 @@
 const allowedOrigins = [
   "http://localhost:3000",
   "https://tcn-lekki-db-frontend-theta.vercel.app",
+  "https://www.tcnlekki.com",
+"https://tcnlekki.com"
 ];
 
 module.exports = allowedOrigins;
